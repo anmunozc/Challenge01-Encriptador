e149fa28de5ef774a2f945df6df975bbaceab9a5
AMC <anmunozc@gmail.com>
2022-09-02 12:57:31 -0400

    Creando Script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// "e" se converte en "enter"
+// "i" se converte en "imes"
+// "a" se converte en "ai"
+// "o" se converte en "ober"
+// "u" se converte en "ufat
+
+function encriptar(){
+    var texto= document.getElementById("ingresarTexto").value.toLowerCase():
+    
+}
