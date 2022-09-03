bc9d1fda2c1e084de48048ee1b8348d1788a6a72
AMC <anmunozc@gmail.com>
2022-09-02 22:29:10 -0400

    funcionando ok con html, css y js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// "a" se converte en "ai"
-// "e" se converte en "enter"
-// "i" se converte en "imes"
-// "o" se converte en "ober"
-// "u" se converte en "ufat
-
 function encriptar(){
-    var texto = document.getElementById("text-input").value.toLowerCase();
-    var txtCrifrado = txtCrifrado.replace(/a/gm,"ai");
-    var txtCrifrado = txtCrifrado.replace(/e/gm,"enter");
-    var txtCrifrado = txtCrifrado.replace(/i/gm,"imes");
-    var txtCrifrado = txtCrifrado.replace(/o/gm,"ober");
-    var txtCrifrado = txtCrifrado.replace(/u/gm,"ufat");
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace("a","ai");
+    var txtCifrado = txtCifrado.replace("e","enter");
+    var txtCifrado = txtCifrado.replace("i","imes");
+    var txtCifrado = txtCifrado.replace("o","ober");
+    var txtCifrado = txtCifrado.replace("u","ufat");
     
    document.getElementById("figura").style.display="none";
    document.getElementById("texto").style.display ="none";
@@ -19,13 +13,13 @@ function encriptar(){
    document.getElementById("copiar").style.display ="inherit";
 }
 
-   function desencriptar(){
-    var texto= document.getElementById("text-input").value.toLowerCase();
-    var txtCrifrado = txtCrifrado.replace(/ai/gm,"a");
-    var txtCrifrado = txtCrifrado.replace(/enter/gm,"e");
-    var txtCrifrado = txtCrifrado.replace(/imes/gm,"i");
-    var txtCrifrado = txtCrifrado.replace(/ober/gm,"o");
-    var txtCrifrado = txtCrifrado.replace(/ufat/gm,"u");
+function desencriptar(){
+    var texto= document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/ai/gm,"a");
+    var txtCrifrado = txtCifrado.replace("enter","e");
+    var txtCifrado = txtCifrado.replace("imes","i");
+    var txtCifrado = txtCifrado.replace("ober","o");
+    var txtCifrado = txtCifrado.replace("ufat","u");
 
     document.getElementById("figura").style.display = "none";
     document.getElementById("texto").style.display = "none";
@@ -36,7 +30,7 @@ function encriptar(){
 
 function copy() {
     var contenido = document.querySelector("#texto2");
-    contenido.Select();
-    document.designMode("copy");
+    contenido.select();
+    document.execCommand("copy");
     alert("Texto Copiado");
 }
